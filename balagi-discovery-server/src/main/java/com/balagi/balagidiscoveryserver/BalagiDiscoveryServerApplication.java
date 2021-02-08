2b0c28eb50e121da341abcfca37e3328f494b9eb
unknown <sadatkarram@gmail.com>
2021-02-08 18:13:02 +0530

    Added discovery server

---
+++ balagi-discovery-server/src/main/java/com/balagi/balagidiscoveryserver/BalagiDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.balagi.balagidiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalagiDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalagiDiscoveryServerApplication.class, args);
+	}
+
+}
