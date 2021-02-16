a00070ab1f2a536436214b21ea5c3e777999bc5e
Ram <sadatkarram@gmail.com>
2021-02-16 22:43:02 +0530

    LEVEL-2: Fault Tolerance,Resilience a) dependencies b) annoation c) circuit breaker pattern using hystrix d) fallback e) hystrix command properties f) bulkHead pattern- keep thread pool seperate for each service.

--- balagi-catalog-service/src/main/java/com/balagi/balagicatalogservice/MovieCatalogController.java
+++ balagi-catalog-service/src/main/java/com/balagi/balagicatalogservice/MovieCatalogController.java
@@ -1,5 +1,6 @@
 package com.balagi.balagicatalogservice;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,46 +9,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.balagi.balagicatalogservice.model.CatalogItem;
 import com.balagi.balagicatalogservice.model.Movie;
 import com.balagi.balagicatalogservice.model.Rating;
 import com.balagi.balagicatalogservice.model.UserRating;
+import com.balagi.balagicatalogservice.service.MovieInfo;
+import com.balagi.balagicatalogservice.service.MovieRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
 	@Autowired
-	RestTemplate restTemplate;
+	MovieInfo movieInfo;
 
 	@Autowired
-	WebClient.Builder webClientBuilder;
+	MovieRating movieRating;
 
 	@GetMapping("/{userId}")
+	// @HystrixCommand(fallbackMethod = "getFallbackCatalog")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		try {
-			UserRating userRatings = restTemplate.getForObject("http://balagi-rating-service/rating/user/User1",
-					UserRating.class);
-			List<Rating> ratings = userRatings.getRatings();
+		UserRating userRatings = movieRating.getRating();
+		List<Rating> ratings = userRatings.getRatings();
 
-			return ratings.stream().map(rating -> {
-				// Movie movie = restTemplate.getForObject("http://localhost:8882/movies/Mov1",
-				// Movie.class);
-				Movie movie = webClientBuilder.build().get().uri("http://balagi-info-service/movies/Mov1").retrieve()
-						.bodyToMono(Movie.class).block();
-
-				return new CatalogItem(movie.getName(), "DescSample", rating.getRating());
-			}).collect(Collectors.toList());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-
+		return ratings.stream()
+				.map(rating -> {
+						Movie movie = movieInfo.getInfo();
+						return new CatalogItem(movie.getName(), "DescSample", rating.getRating());
+				})
+				.collect(Collectors.toList());
 		// return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+	}
+
+	// No longer needed as we add fallback on respective services it was calling
+	public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+		return Arrays.asList(new CatalogItem("No Movie", "fallbackDescSample", "0"));
+
 	}
 
 }
