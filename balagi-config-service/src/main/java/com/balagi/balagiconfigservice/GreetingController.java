62800c8cc0260eebe2dd9cec9a8e60b393395f6f
Ram <sadatkarram@gmail.com>
2021-02-19 01:37:51 +0530

    updated pom with actuator and @refershScope in controller

--- balagi-config-service/src/main/java/com/balagi/balagiconfigservice/GreetingController.java
+++ balagi-config-service/src/main/java/com/balagi/balagiconfigservice/GreetingController.java
@@ -1,14 +1,14 @@
 package com.balagi.balagiconfigservice;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
 	@Value("${my.greeting}")
